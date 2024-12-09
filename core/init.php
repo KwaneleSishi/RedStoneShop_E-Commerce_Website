2ab900c56a8377c11b10398cb3f19b953008353f
KwaneleSishi <kwanelesishi050509@gmail.com>
2024-12-09 13:32:45 +0200

    Adding helpers.php

--- core/init.php
+++ core/init.php
@@ -3,4 +3,17 @@ $db = mysqli_connect('127.0.0.1','root','Kwanele@050509','redstoneshop');
 if(mysqli_connect_error()) {
     echo 'Database Connection Failed with the following errors: '. mysqli_connect_error();
     die();
-} ?>
+} 
+
+// to initialize a start session. This is based on an identifier such as user actions and cookies
+session_start();
+require_once $_SERVER['DOCUMENT_ROOT'].'/tutorial/config.php';
+require_once BASEURL.'helpers/helpers.php';
+
+$cart_id = '';
+if(isset($_COOKIE[CART_COOKIE])) {
+    $cart_id = sanitize($_COOKIE[CART_COOKIE]);
+}
+
+
+?>
