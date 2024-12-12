b431613c81546f0d3ab1ed54bf2f8d553eac0d11
KwaneleSishi <kwanelesishi050509@gmail.com>
2024-12-12 12:07:28 +0200

    the shopping cart functionality is added to the cart.php file

--- cart.php
+++ cart.php
@@ -13,4 +13,27 @@ if($cart_id != '') {
 }
 
 ?>
+<div class="col-nd-12">
+    <div class="row">
+       <h2 class="text-center">My Shopping Cart</h2><hr />
+       <?php if($cart_id == '') ?>
+        <div class="bg-danger">
+            <p class="bg-danger">
+            Your  shopping cart is empty!
+            </p>
 
+        </div>
+        <?php else: ?>
+        <table class="table table-bordered table-condensed table-striped">
+            <thead>
+                <th>#</th>
+                <th>Item</th>
+                <th>Prices</th>
+                <th>Quantity</th>
+                <th>Size</th>
+                <th>Sub-total</th>
+            </thead>
+        </table>
+
+    </div>
+</div>
